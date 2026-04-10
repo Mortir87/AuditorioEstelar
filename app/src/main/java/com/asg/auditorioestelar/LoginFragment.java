396adec7c94d436a293ff7cc615065966cdc1b01
Mortir87 <aitor@blanco.casa>
2026-04-10 17:26:43 +0200

    Se soluciona color de actualizacion al volver a home

--- app/src/main/java/com/asg/auditorioestelar/LoginFragment.java
+++ app/src/main/java/com/asg/auditorioestelar/LoginFragment.java
@@ -14,6 +14,7 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.textfield.TextInputEditText;
 import com.google.gson.Gson;
 
@@ -117,6 +118,8 @@ public class LoginFragment extends Fragment {
                         getParentFragmentManager().beginTransaction()
                                 .replace(R.id.fragment_container, new HomeFragment())
                                 .commit();
+                        BottomNavigationView nav = getActivity().findViewById(R.id.bottom_navigation);
+                        nav.setSelectedItemId(R.id.nav_home);
 
                     } else {
                         // Volvemos al login por el login incorrecto
